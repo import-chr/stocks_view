da21baf3dd7c87d06f8858f84a2cfdaf7de554b9
chris <yosoypro_117@hotmail.com>
2025-12-24 15:21:19 -0600

    chore(tailwind): fix typos miswirtten

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -26,7 +26,7 @@ const UserDropdown = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
+        <Button variant="ghost" className="flex items-center gap-3 text-gray-400 hover:text-yellow-500">
           <Avatar className="h-8 w-8">
             <AvatarImage src="/assets/images/batman460x460.png" />
             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
@@ -62,7 +62,7 @@ const UserDropdown = () => {
           <Navitems />
         </nav>
         <DropdownMenuSeparator className="block sm:hidden bg-gray-600" />
-        <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg=transparent focus:text-yellow-500 transition-colors cursor-pointer">
+        <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
           <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
           Log Out
         </DropdownMenuItem>
