87ce0b9e7fcb2415f2a89e606e71f1c57840187a
chris <yosoypro_117@hotmail.com>
2025-12-26 15:02:39 -0600

    feat(auth): add user prop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,17 @@ import Link from "next/link"
 import Navitems from "@/components/Navitems"
 import UserDropdown from "@/components/UserDropdown"
 
-const Header = () => {
+const Header = ({ user }: { user: User}) => {
   return (
     <header className="sticky top-0 header">
       <div className="container header-wrapper">
         <Link href="/">
-          <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
+          <Image src="/assets/icons/logo.svg" alt="Stox logo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
         </Link>
         <nav className="hidden sm:block">
           <Navitems />
         </nav>
-        <UserDropdown />
+        <UserDropdown user={user} />
       </div>
     </header>
   )
